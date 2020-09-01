c4f0c8acc7ba59d995883b9dcb68721dac3ab750
Eduardo Perez <edujpp1@gmail.com>
2020-08-31 21:26:00 -0300

    The user's IP is saved in a cookie, it redirects to /hello from the root directory

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
+def index():
   user_ip = request.remote_addr
+
+  response = make_response(redirect('/hello'))
+  response.set_cookie('user_ip', user_ip) # se guarda en una cookie la IP del usuario
+
+  return response
+
+
+@app.route('/hello')
+def hello():
+  user_ip = request.cookies.get('user_ip') # Se obtiene de las cookies la IP del usuario
+
   return f'Hello World Flask! Your IP is {user_ip}'
