ba6fb69c2fe0aefabe2ff7e07ca04e2d3b6cde88
Eduardo Perez <edujpp1@gmail.com>
2020-09-05 15:37:30 -0300

    A to do list is displayed using inheritance of templates and macros

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['Comprar cafe', 'Solicitud de compra', 'Entregar video al productor']
 
 @app.route('/')
 def index():
   user_ip = request.remote_addr
 
   response = make_response(redirect('/hello'))
-  response.set_cookie('user_ip', user_ip) # se guarda en una cookie la IP del usuario
+  response.set_cookie('user_ip', user_ip) # The user's IP is saved in the cookie
 
   return response
 
 
 @app.route('/hello')
 def hello():
-  user_ip = request.cookies.get('user_ip') # Se obtiene de las cookies la IP del usuario
+  user_ip = request.cookies.get('user_ip') # The user's IP is obtained from the cookie
+  context = {
+    'user_ip': user_ip,
+    'todos': todos,
+  }
 
-  return render_template('hello.html', user_ip=user_ip)
+  # The context varible is expanded for pass every key:value as single variables
+  return render_template('hello.html', **context)
