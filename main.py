9eba7c584a96733fd6b1c18706e714a1f58186f4
Eduardo Perez <edujpp1@gmail.com>
2020-09-06 16:55:43 -0300

    The POST method was configurated so that Flask-WTF can send the form. The username varible in the hello.html themplate was modified to capitalized the word. This was with the use of the filters with the character pipe '|'

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
@@ -8,7 +8,7 @@ from wtforms.validators import DataRequired
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-# Key for generate a security session y flask
+# Key for generate a security session in flask
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['Comprar cafe', 'Solicitud de compra', 'Entregar video al productor']
@@ -47,15 +47,26 @@ def index():
   return response
 
 
-@app.route('/hello')
+# By default the flask routes allow the GET method, but when we want allow a POST method we have to declarte both in the list
+# It isn't necesary when the route is just going to accept the GET method
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
   user_ip = session.get('user_ip') # The user's IP is obtained from the cookie
   login_form = LoginForm()
+  username = session.get('username')
+
   context = {
     'user_ip': user_ip,
     'todos': todos,
-    'login_form': login_form
+    'login_form': login_form,
+    'username': username
   }
+
+  if login_form.validate_on_submit():
+    username = login_form.username.data
+    session['username'] = username
+
+    return redirect(url_for('index'))
 
   # The context varible is expanded for pass every key:value as single variables
   return render_template('hello.html', **context)
