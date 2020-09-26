0ba6b1dd0b9602e874cf8b213751008f023a34b5
Eduardo PÃerez <eperez@prismamp.com>
2020-09-26 20:34:48 -0300

    It was implemented the logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,9 @@
 from flask import render_template, session, redirect, flash, url_for
+from flask_login import login_user, login_required, logout_user
 
 from app.forms import LoginForm
+from app.firestore_service import get_user
+from app.models import UserModel, UserData
 
 from . import auth
 
@@ -13,10 +16,37 @@ def login():
 
   if login_form.validate_on_submit():
     username = login_form.username.data
-    session['username'] = username
+    password = login_form.password.data
 
-    flash('Username registered successfully')
+    user_doc = get_user(username)
+
+    if user_doc.to_dict() is not None:
+      password_from_db = user_doc.to_dict()['password']
+
+      if password == password_from_db:
+        user_data = UserData(username, password)
+        user = UserModel(user_data)
+
+        login_user(user)
+
+        flash('Welcome again!')
+
+        redirect(url_for('hello'))
+      else:
+        flash('The information doesn\'t match!')
+    else:
+      flash('The user doesn\'t exist!')
+
 
     return redirect(url_for('index'))
 
   return render_template('login.html', **context)
+
+
+@auth.route('logout')
+@login_required
+def logout():
+  logout_user()
+  flash('Come back soon!')
+
+  return redirect(url_for('auth.login'))
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import unittest
 from flask import request, make_response, redirect, render_template, session, url_for, flash
 #from flask_bootstrap import Bootstrap
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 from app import create_app
 from app.forms import LoginForm
@@ -51,7 +51,7 @@ def index():
 @login_required
 def hello():
   user_ip = session.get('user_ip') # The user's IP is obtained from the cookie
-  username = session.get('username')
+  username = current_user.id
 
   context = {
     'user_ip': user_ip,
